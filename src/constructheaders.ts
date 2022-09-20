8e8249b144af43f633a19d9506c2342d80e39c77
Brady Bellini <bbellini@pm.me>
2022-09-19 20:39:41 -0700

    Create constructheaders.ts

---
+++ src/constructheaders.ts
@@ -0,0 +1,20 @@
+export class ConstructHeaders {
+    
+    public async createHeader() {
+        
+    }
+    
+    private async check_etag() {
+        
+    }
+    
+    private async check_max_age() {
+        
+    }
+    
+    private async check_cache() {
+        
+    }
+    
+    
+}   
